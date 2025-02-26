e139d9fce8f9327c39271cc8fabbee3381134c46
Adid28 <adid66741@gmail.com>
2025-02-25 23:16:08 -0500

    Agregada funcionalidad de resta

--- app.py
+++ app.py
@@ -1,13 +1,9 @@
-def hola_mundo():
-    mensaje = "Hola, Mundo"
-    print(mensaje)
-    with open("mensaje.txt", "w") as archivo:
-        archivo.write(mensaje)
-
-# Llamar a la función
-hola_mundo()
-
 def suma(a, b):
     return a + b
 
 print("La suma de 5 + 3 es:", suma(5, 3))
+
+def resta(a, b):
+    return a - b
+
+print(f"La resta de 10 - 4 es: {resta(10, 4)}")
